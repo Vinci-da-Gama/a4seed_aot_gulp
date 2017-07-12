d50b2db22454cc4bc1691dd51c830f5f3fb1c94c
Occ Coo <kylingourmand@gmail.com>
2017-07-13 05:51:39 +1000

    adjust_sass

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,5 +1,4 @@
 import { join } from 'path';
-
 import { SeedConfig } from './seed.config';
 // import { ExtendPackages } from './seed.config.interfaces';
 
@@ -9,75 +8,75 @@ import { SeedConfig } from './seed.config';
  */
 export class ProjectConfig extends SeedConfig {
 
-  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
-  // external fonts
-  FONTS_DEST = `${this.APP_DEST}/fonts`;
-  FONTS_SRC = [
-      'node_modules/bootstrap/dist/fonts/**'
-  ];
+	PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+	// external fonts
+	FONTS_DEST = `${this.APP_DEST}/fonts`;
+	FONTS_SRC = [
+		'node_modules/bootstrap/dist/fonts/**'
+	];
 
-  constructor() {
-    super();
-    // this.APP_TITLE = 'Put name of your app here';
-    // this.GOOGLE_ANALYTICS_ID = 'Your site's ID';
+	constructor() {
+		super();
+		// this.APP_TITLE = 'Put name of your app here';
+		// this.GOOGLE_ANALYTICS_ID = 'Your site's ID';
 
-    /* Enable typeless compiler runs (faster) between typed compiler runs. */
-    this.TYPED_COMPILE_INTERVAL = 5;
+		/* Enable typeless compiler runs (faster) between typed compiler runs. */
+		this.TYPED_COMPILE_INTERVAL = 5;
 
-    // Add `NPM` third-party libraries to be injected/bundled.
-    this.NPM_DEPENDENCIES = [
-      ...this.NPM_DEPENDENCIES,
-      {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-      // {src: 'lodash/lodash.min.js', inject: 'libs'},
-      {src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs'},
-      {src: 'bootstrap/dist/css/bootstrap.min.css', inject: true}
-    ];
+		// Add `NPM` third-party libraries to be injected/bundled.
+		this.NPM_DEPENDENCIES = [
+			...this.NPM_DEPENDENCIES,
+			{ src: 'jquery/dist/jquery.min.js', inject: 'libs' },
+			// {src: 'lodash/lodash.min.js', inject: 'libs'},
+			{ src: 'bootstrap/dist/js/bootstrap.min.js', inject: 'libs' },
+			{ src: 'bootstrap/dist/css/bootstrap.min.css', inject: true }
+		];
 
-    // Add `local` third-party libraries to be injected/bundled.
-    this.APP_ASSETS = [
-      // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
-      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
-    ];
+		// Add `local` third-party libraries to be injected/bundled.
+		this.APP_ASSETS = [
+			// {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
+			// {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+		];
 
-    this.SCSS_SRC = `${this.APP_SRC}/scss`;
 		this.ENABLE_SCSS = true;
+		this.SCSS_SRC = `${this.APP_SRC}/scss`;
 
-    this.ROLLUP_INCLUDE_DIR = [
-      ...this.ROLLUP_INCLUDE_DIR,
-      //'node_modules/moment/**'
-    ];
+		this.ROLLUP_INCLUDE_DIR = [
+			...this.ROLLUP_INCLUDE_DIR,
+			//'node_modules/moment/**'
+		];
 
-    this.ROLLUP_NAMED_EXPORTS = [
-      ...this.ROLLUP_NAMED_EXPORTS,
-      //{'node_modules/immutable/dist/immutable.js': [ 'Map' ]},
-    ];
+		this.ROLLUP_NAMED_EXPORTS = [
+			...this.ROLLUP_NAMED_EXPORTS,
+			//{'node_modules/immutable/dist/immutable.js': [ 'Map' ]},
+		];
 
-    // Add packages (e.g. ng2-translate)
-    // let additionalPackages: ExtendPackages[] = [{
-    //   name: 'ng2-translate',
-    //   // Path to the package's bundle
-    //   path: 'node_modules/ng2-translate/bundles/ng2-translate.umd.js'
-    // }];
-    //
-    // this.addPackagesBundles(additionalPackages);
+		// Add packages (e.g. ng2-translate)
+		// let additionalPackages: ExtendPackages[] = [{
+		//   name: 'ng2-translate',
+		//   // Path to the package's bundle
+		//   path: 'node_modules/ng2-translate/bundles/ng2-translate.umd.js'
+		// }];
+		//
+		// this.addPackagesBundles(additionalPackages);
 
-    // Add Firebase configuration to SystemJS
-    this.addPackageBundles({
-      name: 'firebase',
-      path: 'node_modules/firebase/',
-      packageMeta: {
-        main: 'firebase-browser.js',
-        defaultExtension: 'js'
-      }
-    });
+		// Add Firebase configuration to SystemJS
+		this.addPackageBundles({
+			name: 'firebase',
+			path: 'node_modules/firebase/',
+			packageMeta: {
+				main: 'firebase-browser.js',
+				defaultExtension: 'js'
+			}
+		});
 
-    /* Add proxy middleware */
-    // this.PROXY_MIDDLEWARE = [
-    //   require('http-proxy-middleware')('/api', { ws: false, target: 'http://localhost:3003' })
-    // ];
+		/* Add proxy middleware */
+		// this.PROXY_MIDDLEWARE = [
+		//   require('http-proxy-middleware')('/api', { ws: false, target: 'http://localhost:3003' })
+		// ];
 
-    /* Add to or override NPM module configurations: */
-    // this.PLUGIN_CONFIGS['browser-sync'] = { ghostMode: false };
-  }
+		/* Add to or override NPM module configurations: */
+		// this.PLUGIN_CONFIGS['browser-sync'] = { ghostMode: false };
+	}
 
 }
